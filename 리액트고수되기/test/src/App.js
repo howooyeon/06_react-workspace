ced377904b5e9ccb9b379c671e91b0fdee22f335
hoyeon <aviationlovers@naver.com>
2023-09-18 07:10:45 +0900

    commit

--- 리액트고수되기/test/src/App.js
+++ 리액트고수되기/test/src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [friend, setFriend] = useState(data);
+  const [inputs, setInputs] = useState({
+    id: '',
+    name: '',
+    birthday:''
+  })
   return (
     <div className="App">
-      const [friend, setFriend] = useState(data);
+      
     </div>
   );
 }
